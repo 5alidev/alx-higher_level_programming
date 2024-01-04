18eb23c66245baa70f53da548cc9ce8b6454c625
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-04 15:09:53 -0800

    infinite addition v2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 from sys import argv
 if __name__ == "__main__":
     sum = 0
-    if len(argv) > 1:
-        for arg in argv[1:]:
-            sum += int(arg)
-        print("{:d}".format(sum))
+    for arg in argv[1:]:
+        sum += int(arg)
+    print("{:d}".format(sum))
