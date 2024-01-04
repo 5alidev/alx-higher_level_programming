be468297d965cf7493e9a43d54d12516809050b8
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-04 14:58:16 -0800

    print args v2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
-inc = 0
-if len(argv) <= 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("{:d} argument:".format(len(argv) - 1))
-    print(f"1: {argv[1]}")
-else:
-    print("{:d} arguments:".format(len(argv) - 1))
-    for arg in argv[1:]:
-        inc += 1
-        print(f"{inc}: {arg}")
+if __name__ == "__main__":
+    inc = 0
+    if len(argv) <= 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+        print(f"1: {argv[1]}")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for arg in argv[1:]:
+            inc += 1
+            print(f"{inc}: {arg}")
