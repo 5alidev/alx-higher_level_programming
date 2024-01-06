40e623b3983d7b0a6caab495324899c12fcf8fac
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-06 12:42:02 -0800

    removes ell c chars

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    new_str = ""
+    if my_string:
+        for char in my_string:
+            if char != "C" and char != "c":
+                new_str += char
+    return new_str
