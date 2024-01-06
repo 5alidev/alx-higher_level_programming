2d4813abc9e4d51797be7310560e07d6ba131458
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-06 12:45:11 -0800

    remove if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_str = ""
-    if my_string:
-        for char in my_string:
-            if char != "C" and char != "c":
-                new_str += char
+    for char in my_string:
+        if char != "C" and char != "c":
+            new_str += char
     return new_str
