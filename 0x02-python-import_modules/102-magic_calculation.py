631b19477830fcc23e4aa7fd6feaa21696144aa6
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-08 07:35:30 -0800

    change import way for add and sub

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 def magic_calculation(a, b):
-    add = __import__('magic_calculations_102').add
-    sub = __import__('magic_calculations_102').sub
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+
     if a < b:
         c = add(a, b)
         for n in range(4, 6):
