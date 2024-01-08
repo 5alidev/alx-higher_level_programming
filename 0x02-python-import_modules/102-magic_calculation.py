2f8dad5f1ad12fc13b660e78255bf6a4abe60658
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-08 07:32:37 -0800

    magic calculation v2

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    add = magic_calculations_102.add
-    sub = magic_calculations_102.sub
+    add = __import__('magic_calculations_102').add
+    sub = __import__('magic_calculations_102').sub
     if a < b:
         c = add(a, b)
         for n in range(4, 6):
