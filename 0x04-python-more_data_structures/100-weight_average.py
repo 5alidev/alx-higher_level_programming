1e09f638f72314b8d27f34340b9a70065d16bc9a
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-12 02:11:23 -0800

    weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """weighted average of all integers tuple"""
+    avg = 0
+    n = 0
+    if len(my_list) <= 0:
+        return 0
+    for i, tup in enumerate(my_list):
+        avg += (my_list[i][0] * my_list[i][1])
+        n += my_list[i][1] 
+    return avg / n
