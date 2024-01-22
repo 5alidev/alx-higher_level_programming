9f75f92549ab29d7b1dab510e1751e3f388684d0
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-22 04:20:41 -0800

    add Readme file and a function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list."""
+    count = 0
+    for i, value in enumerate(my_list):
+        try:
+            if i < x:
+                print(f"{my_list[i]}", end="")
+                count += 1
+        except:
+            pass
+    print()
+    return count
