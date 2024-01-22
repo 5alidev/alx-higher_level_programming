1c8e240eae362ca3e7f695b01dc0cf22072e8838
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-22 05:54:10 -0800

    adjust code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,10 +4,9 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-        print("Inside result: {}".format(result))
     except Exception:
         result = None
-        print("Inside result: {}".format(result))
     finally:
-        return result
+        print("Inside result: {}".format(result))
+    return result
 
