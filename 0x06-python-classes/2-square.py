190db9baefc665703a4d6bf84fe6d3d75e492834
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-23 07:59:05 -0800

    raise errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define Square class with a private attribute"""
+
+
+class Square:
+    """Square class with private size attribute
+
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size=0):
+        """init to create new instance of square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
