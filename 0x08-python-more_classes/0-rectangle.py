ecf5a396e4261856bbe487720d399adb264c8dfd
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-29 16:19:10 -0800

    add readme file and a simple rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+simple module to create a rectangle class
+'''
+
+
+class Rectangle:
+    """empty rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Alx Python **More Classes** Tasks.
