6e9e76efdaed25dbf8b38109c7976acbb4668029
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-05 08:14:58 -0800

    implement area method and define str

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Rectangle class'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    class Rectangle that inherits from BaseGeometry class
+    '''
+
+    def __init__(self, width, height):
+        '''
+        Rectangle constructor
+
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        '''
+
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        '''
+        default str method
+        '''
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
