9a25534a061eec6a2ab4734c4dc93e50ead34ae0
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-05 04:21:52 -0800

    fix doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+'''lookup() function'''
+
+
 def lookup(obj):
     '''
     return a list o available attributes and methods of obj
+
+    Args:
+        obj: Object
+
+    Returns:
+        list: available attributes and methods of obj
     '''
     return dir(obj)
