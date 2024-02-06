e4ebbf307056baf7fe2da5d7e65fec044a27481d
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-06 13:14:16 -0800

    update to_json Student method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+class Student
+'''
+
+
+class Student:
+    '''
+    Student class with 3 public instance attributes
+    and a public method
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Student Constructor
+
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        retrieves a dictionary representation of a Student instance
+        '''
+
+        if attrs is None:
+            return vars(self)
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
