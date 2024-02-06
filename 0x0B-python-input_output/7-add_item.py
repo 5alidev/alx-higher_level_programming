5deb49ce1ca0c2ee14de8077b702ee28ec3ffd1b
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-06 07:56:52 -0800

    add args to a python list, then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+jsonList = []
+if os.path.exists("add_item.json"):
+    jsonList = load_from_json_file("add_item.json")
+
+for arg in sys.argv[1:]:
+    jsonList.append(arg)
+
+save_to_json_file(jsonList, "add_item.json")
