eaf3ba463f286c4742f82b701e8fe28bb11008c4
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-08 18:19:35 -0700

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,10 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.sort();
-  console.log(arr.reverse()[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const max = Math.max(...arr);
+  const index = arr.indexOf(max);
+  arr.splice(index, 1);
+  const secondMax = Math.max(...arr);
+  console.log(secondMax);
 }
