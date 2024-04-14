172f49faaec8dedb152d088925af56f172b1ab70
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-14 16:21:11 +0100

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N"""
+# Usage: ./1-filter_states.py username password db_name
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    name = sys.argv[4]
+    con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = con.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE %s
+                ORDER BY states.id ASC""", (name, ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    con.close()
