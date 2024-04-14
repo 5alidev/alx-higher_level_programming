e51b1dea3b07e9ece55b7a2171199d114699c8f7
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-14 13:11:08 +0100

    add sql test file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # script that lists all states from the database hbtn_0e_0_usa
-# Usage: /0-select_states.py username password db_name
+# Usage: ./0-select_states.py username password db_name
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     [print(state) for state in cur.fetchall()]
