3a0cb98a02baf15fde59de66fa394553d694cc83
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-15 01:11:19 +0100

    import ForeignKey

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' module that contains the class definition of a State '''
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
