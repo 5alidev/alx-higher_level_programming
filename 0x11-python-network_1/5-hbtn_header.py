f40c359307999fd936341931fcec8ca938444d74
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-26 14:37:10 -0700

    use headers.get()

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    var = response.headers['X-Request-Id']
+    var = response.headers.get("X-Request-Id")
     print(var)
