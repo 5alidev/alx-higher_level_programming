09265a21ddec852251cc224b44f8173188e38d80
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-26 15:42:05 -0700

    Error Code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,9 +9,8 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
-    except requests.HTTPError as e:
+    response = requests.get(url)
+    if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
