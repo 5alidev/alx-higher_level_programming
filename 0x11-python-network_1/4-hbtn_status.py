f1d9d2df56c672e0f957fcb62760d9659fd8fa84
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-26 12:17:31 -0700

    remove check if 200

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,6 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    if response.status_code == 200:
-        print("Body response:")
-        print("\t- type {}".format(type(response.text)))
-        print("\t- content {}".format(response.text))
+    print("Body response:")
+    print("\t- type {}".format(type(response.text)))
+    print("\t- content {}".format(response.text))
