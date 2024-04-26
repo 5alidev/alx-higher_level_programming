68b1fe4605973110170d63b67f9ecaf5f583cef0
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-26 16:26:40 -0700

    use .get

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     url = "https://api.github.com/users/{}".format(sys.argv[1])
     response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     data = response.json()
-    print(data["id"])
+    print(data.get("id"))
